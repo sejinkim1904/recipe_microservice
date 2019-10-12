df26c33eaf94de07dbd1231f8beef51559d3a68a
Sejin Kim <froydroyce@gmail.com>
2019-10-12 15:15:13 -0600

    Create recipe table

---
+++ migrations/20191012211249-create-recipe.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Recipes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      uri: {
+        type: Sequelize.STRING
+      },
+      calories: {
+        type: Sequelize.FLOAT
+      },
+      totalTime: {
+        type: Sequelize.FLOAT
+      },
+      ingredients: {
+        type: Sequelize.ARRAY(Sequelize.STRING)
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Recipes');
+  }
+};
