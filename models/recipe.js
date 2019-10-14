51f8b1be4c90f807f9d65cee8c28fd9949870432
Sejin Kim <froydroyce@gmail.com>
2019-10-14 13:25:31 -0600

    Add attributes for yield, foodType, and url
    Co-Authored-By: S. Mainar <smainar@users.noreply.github.com>

--- models/recipe.js
+++ models/recipe.js
@@ -2,10 +2,12 @@
 module.exports = (sequelize, DataTypes) => {
   const Recipe = sequelize.define('Recipe', {
     name: DataTypes.STRING,
+    foodType: DataTypes.STRING,
     image: DataTypes.STRING,
-    uri: DataTypes.STRING,
+    url: DataTypes.STRING,
     calories: DataTypes.FLOAT,
     totalTime: DataTypes.FLOAT,
+    yield: DataTypes.FLOAT,
     ingredients: DataTypes.ARRAY(DataTypes.STRING)
   }, {});
   Recipe.associate = function(models) {
