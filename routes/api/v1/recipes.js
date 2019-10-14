bf36b1c73bdffa903b1666c9410e0aa2242d3eaa
Sejin Kim <froydroyce@gmail.com>
2019-10-14 16:14:53 -0600

    Add route controller/action for GET time_search request
    Co-Authored-By: S. Mainar <smainar@users.noreply.github.com>

--- routes/api/v1/recipes.js
+++ routes/api/v1/recipes.js
@@ -93,4 +93,49 @@ router.get('/calorie_search', async (req, res, next) => {
     });
 });
 
+router.get('/time_search', async (req, res, next) => {
+  res.setHeader('Content-Type', 'application/json')
+  let createdRecipes = []
+
+  await recipe.findAll({
+    where: {
+      foodType: req.query.q,
+      totalTime: { [Op.lte]: req.query.max },
+    }
+  })
+    .then(async recipes => {
+      if (recipes.length === 0) {
+        let edamam = await new edamamService(req.query.q);
+        let max = req.query.max
+
+        await edamam.getRecipesByTime(max)
+          .then(async edamamResponse => {
+            let edamamRecipes = await edamamResponse.hits;
+            for (let i = 0; i < edamamRecipes.length; i++) {
+              let result = edamamRecipes[i];
+              await recipe.create({
+                name: result.recipe.label,
+                foodType: req.query.q,
+                image: result.recipe.image,
+                url: result.recipe.url,
+                calories: result.recipe.calories,
+                totalTime: result.recipe.totalTime,
+                yield: result.recipe.yield,
+                ingredients: result.recipe.ingredientLines
+              })
+                .then(async recipe =>
+                  createdRecipes.push(recipe)
+                );
+            };
+          });
+        res.status(200).send(createdRecipes)
+        return;
+      };
+      res.status(200).send(recipes)
+    })
+    .catch(async error => {
+      res.status(500).send( {error} )
+    });
+});
+
 module.exports = router;
